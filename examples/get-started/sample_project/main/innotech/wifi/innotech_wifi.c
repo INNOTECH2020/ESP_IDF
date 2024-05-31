573fdcbfcc2595cc16632843e5923ccd59903ba6
zhangqiang012 <542886873@qq.com>
2024-05-31 15:52:25 +0800

    modify wifi connect and scan merge

--- examples/get-started/sample_project/main/innotech/wifi/innotech_wifi.c
+++ examples/get-started/sample_project/main/innotech/wifi/innotech_wifi.c
@@ -542,6 +542,11 @@ void wifi_init_sta(wifi_param_t wifi)
 
 void innotech_wifi_init(void)
 {
+    s_wifi_event_group = xEventGroupCreate();
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
     if(wifi_config.flag == WIFI_CONFIG_SUC)
     {
         wifi_init_sta(wifi_config);
@@ -565,7 +570,6 @@ void innotech_triplet_init(void)
 
 void innotech_netif_init(void)
 {
-    s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
@@ -574,10 +578,6 @@ void innotech_netif_init(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
